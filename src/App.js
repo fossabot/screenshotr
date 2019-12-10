84d592cc21a95c053bfb1543eecbf6079d920187
csandman <csandvik@uvm.edu>
2019-12-10 11:03:45 -0500

    moved screenshot export into separate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useContext, useRef } from 'react';
-import domtoimage from 'dom-to-image';
-import FileSaver from 'file-saver';
 import useComponentSize from '@rehooks/component-size';
 import { Checkboard } from 'react-color/lib/components/common';
 import Header from './components/header/header';
@@ -8,22 +6,18 @@ import Sidebar from './components/sidebar/sidebar';
 import BrowserWindow from './components/browser-window/browser-window';
 import OptionsContext from './contexts/options-context';
 import OutputContext from './contexts/output-context';
+import { downloadScreenshot } from './util/screenshot';
 import './App.scss';
-
-// IF THERE IS AN IMAGE ALREADY THERE WHEN THE RESOLUTION IS CHANGED, REPULL THE IMAGE
-// NOT THE FAVICON, JUST THE IMAGE
 
 function App() {
   const { options } = useContext(OptionsContext);
-  const { getScreenshot } = useContext(OutputContext);
+  const { getScreenshot, cleanURL } = useContext(OutputContext);
   const { outputWidth, background } = options;
 
   const [inputVal, setInputVal] = useState('');
 
-  const cleanUrl = inputVal
-    .replace(/https?:\/\//, '')
-    .split('/')[0]
-    .trim();
+  const exportRef = useRef(null);
+  const exportSize = useComponentSize(exportRef);
 
   const updateInputVal = e => {
     setInputVal(e.target.value);
@@ -31,35 +25,15 @@ function App() {
 
   const getImage = async e => {
     e.preventDefault();
-
     getScreenshot(inputVal);
   };
 
-  const exportRef = useRef(null);
-  const exportSize = useComponentSize(exportRef);
-
-  const downloadScreenshot = async () => {
-    const filenameURL = cleanUrl.split('.');
-    const filename = filenameURL[filenameURL.length - 2];
-    const node = exportRef.current;
-    const scale = exportSize.width / node.offsetWidth;
-    const dataURL = await domtoimage.toPng(node, {
-      height: node.offsetHeight * scale,
-      width: node.offsetWidth * scale,
-      filter: el => {
-        console.log(el);
-        console.log(el.classList);
-        return !el?.classList?.contains('do-not-export');
-      },
-      style: {
-        transform: `scale(${scale})`,
-        transformOrigin: 'top left',
-        width: `${node.offsetWidth}px`,
-        height: `${node.offsetHeight}px`,
-        backgroundImage: 'none'
-      }
-    });
-    FileSaver.saveAs(dataURL, `${filename}.png`);
+  const handleDownloadClick = () => {
+    const filenameArr = cleanURL.split('.');
+    const filename = filenameArr[filenameArr.length - 2];
+    const exportNode = exportRef.current;
+    const exportWidth = exportSize.width;
+    downloadScreenshot(filename, exportNode, exportWidth);
   };
 
   return (
@@ -71,7 +45,7 @@ function App() {
       />
       <section className="app-body">
         <Sidebar
-          handleDownloadClick={downloadScreenshot}
+          handleDownloadClick={handleDownloadClick}
           exportSize={exportSize}
         />
         <article className="app-body-content">
---
+++ src/util/screenshot.js
@@ -0,0 +1,26 @@
+import { toPng } from 'dom-to-image';
+import { saveAs } from 'file-saver';
+
+export const downloadScreenshot = async (filename, exportNode, exportWidth) => {
+  // https://github.com/tsayen/dom-to-image/issues/69#issuecomment-486146688
+  const scale = exportWidth / exportNode.offsetWidth;
+  const dataURL = await toPng(exportNode, {
+    height: exportNode.offsetHeight * scale,
+    width: exportNode.offsetWidth * scale,
+    filter: el => {
+      console.log(el);
+      console.log(el.classList);
+      return !el?.classList?.contains('do-not-export');
+    },
+    style: {
+      transform: `scale(${scale})`,
+      transformOrigin: 'top left',
+      width: `${exportNode.offsetWidth}px`,
+      height: `${exportNode.offsetHeight}px`,
+      backgroundImage: 'none'
+    }
+  });
+  saveAs(dataURL, `${filename}.png`);
+};
+
+export const test = () => {};
