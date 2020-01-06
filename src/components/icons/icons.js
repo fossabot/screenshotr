0eff5ceb85927e9a2197707f1a7d940f0cb0994c
csandman <csandvik@uvm.edu>
2020-01-06 16:31:30 -0500

    Moved github button to body/ download button to bottom of sidebar - closes #52

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -105,3 +105,19 @@ export const RightArrowIcon = props => (
     <polyline points="12 5 19 12 12 19" />
   </Svg>
 );
+
+export const DownloadIcon = props => (
+  <Svg
+    size={24}
+    {...props}
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    fill="none"
+    stroke="currentColor"
+  >
+    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+    <polyline points="7 10 12 15 17 10" />
+    <line x1="12" y1="15" x2="12" y2="3" />
+  </Svg>
+);
